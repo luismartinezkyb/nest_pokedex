33d00cd09062494656ed25efa65d6a0ccd65c295
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-12-06 14:24:08 -0600

    (feat): Implementación de env file with condigModule package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,21 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(__dirname, '..', '.env'),
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/pokedex'),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     PokemonModule,
     CommonModule,
     SeedModule,
